d1ba75788df0e95a8c4aed74c091d8464fb0811a
LightedExe <Thundermagicreal@gmail.com>
2024-11-20 12:36:26 +0800

    better logging

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ export default async ({ req, res, log, error }) => {
 
   const clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   // for user ip info https://ipinfo.io/{ip address}/json
-  log(req.bodyText)
-  log(clientIP)
+  log('body ' + req.bodyText)
+  log('ip ' + clientIP)
   return res.empty()
 };
