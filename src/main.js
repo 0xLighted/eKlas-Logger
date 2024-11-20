1483d9cd93c9025da31128dd7bdda1beeaa0b82b
LightedExe <Thundermagicreal@gmail.com>
2024-11-20 12:42:44 +0800

    remove post check temporarily

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import { Databases } from 'node-appwrite'
 
 export default async ({ req, res, log, error }) => {
-  if (req.method != 'GET') {
-    return res.status(405)
-  }
-
   const clientIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   // for user ip info https://ipinfo.io/{ip address}/json
   log('body ' + req.bodyText)
