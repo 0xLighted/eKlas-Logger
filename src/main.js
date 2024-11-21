e22a5f24f244e5a4312afcf54016eef6e4a1b47f
LightedExe <Thundermagicreal@gmail.com>
2024-11-21 12:05:58 +0800

    doing random stuff

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ export default async ({ req, res, log, error }) => {
       Matric: req.bodyJson['user']['matric'],
       Name: req.bodyJson['user']['name'],
       device: [{
-        $id: req.bodyJson['device']['_system'],
+        $id: "testing",
         Datetime: req.bodyJson['device']['datetime'],
         System: req.bodyJson['device']['_system'],
         Browser: req.bodyJson['device']['browser'],
