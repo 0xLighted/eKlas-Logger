a44d4c56e0ac455372377547d60eff81af6d4db6
LightedExe <Thundermagicreal@gmail.com>
2024-11-22 16:57:01 +0800

    Revert back to main

--- src/main.js
+++ src/main.js
@@ -1,35 +1,111 @@
-import { Client, Users } from 'node-appwrite';
+import { Databases, Client } from 'node-appwrite'
 
-// This Appwrite function will be executed every time your function is triggered
-export default async ({ req, res, log, error }) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
-  const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
-
+function validateBody(bodyJson) {
+  const userBody = [ "name", "matric", "phpsess" ].toString()
+  const deviceBody = [ "browser", "screen", "viewport", "CPU", "memory", "timezone","system" ].toString()
+  
   try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
+    if (Object.keys(bodyJson['user']).toString() != userBody || Object.keys(bodyJson['device']).toString() != deviceBody)
+      { return false }
+  } catch { return false }
+
+  return true
+}
+
+
+export default async ({ req, res, log, error }) => {
+  if (req.method != 'POST' || req.path != '/') {
+    error('Method not allowed: ' + req.method);
+    return res.json({success: false, message: "Method not allowed, Please send POST"});
+  }
+
+  if (!validateBody(req.bodyJson)) {
+    error('Invalid data object: ' + req.bodyText);
+    return res.json({success: false, message: "Invalid data object"});
+  }
+
+  // Appwrite project
+  const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID);
+    
+  const database = new Databases(client);
+  
+  // Grab user IP and IP details
+  const clientIP = req.headers['x-forwarded-for'] ? req.headers['x-forwarded-for'].split(',')[0] : req.socket.remoteAddress;
+  const ipinfo = (await (await fetch(`https://ipinfo.io/${clientIP}/json`)).json());
+  const sanitizedMatric = req.bodyJson['user']['matric']
+    .trim()
+    .replaceAll(' ', '_')
+    .slice(0, 36);
+
+  const userData = {
+    Matric: req.bodyJson['user']['matric'],
+    Name: req.bodyJson['user']['name'],
+    LatestPHPSession: req.bodyJson['user']['phpsess'],
+  }
+  const deviceData = {
+    System: req.bodyJson['device']['system'],
+    Browser: req.bodyJson['device']['browser'],
+    Screen: req.bodyJson['device']['screen'],
+    Viewport: req.bodyJson['device']['viewport'],
+    CPU: req.bodyJson['device']['CPU'],
+    RAM: req.bodyJson['device']['memory'],
+    Timezone: req.bodyJson['device']['timezone'],
+  }
+  const IPData = {
+    Address: ipinfo['ip'],
+    Country: ipinfo['country'],
+    City: ipinfo['city'],
+    Region: ipinfo['region'],
+    Coordinates: ipinfo['loc'],
+    ISP: ipinfo['org'],
+    Postal: ipinfo['postal'],
+    Hostname: ipinfo['hostname']
+  } 
+  
+  // Create or update user document
+  try {
+    const userDoc = await database.getDocument('Logger', 'User', sanitizedMatric);
+
+    //Update user session and logintime
+    await database.updateDocument('logger', 'User', sanitizedMatric, {
+      LatestPHPSession: userData['LatestPHPSession'],
+      LatestLogin: userData['LatestLogin']
+    });
+
+    // Check if Device already registered, if not append new Device to user document
+    if (userDoc['Devices'].filter(device => device['System'] === req.bodyJson['device']['system']).length == 0) {
+      await database.updateDocument('logger', 'User', sanitizedMatric, {
+        Devices: [...userDoc['Devices'], deviceData]
+      });
+
+      log(`Device "${req.bodyJson['device']['system']}" successfully registered`);
+    } else { log(`Device "${req.bodyJson['device']['system']}" already registered`); }
+
+    // Check if IP already registered, if not append new IP to user document
+    if (userDoc['IPs'].filter(ip => ip['Address'] === ipinfo['ip']).length == 0) {
+      await database.updateDocument('logger', 'User', sanitizedMatric, {
+        IPs: [...userDoc['IPs'], IPData]
+      });
+
+      log(`User IP "${ipinfo['ip']}" successfully registered`);
+    } else { log(`User IP "${ipinfo['ip']}" already registered`); }
+    
+    log(`User ${sanitizedMatric} updated successfully`);
+    return res.json({success: true, message: `User ${sanitizedMatric} updated successfully`});
+
   } catch(err) {
-    error("Could not list users: " + err.message);
-  }
+    // If user doesnt exist, the function will raise an error, and create new document with data
+    userData['Devices'] = [deviceData];
+    userData['IPs'] = [IPData];
+    await database.createDocument('Logger', 'User', sanitizedMatric, userData);
 
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
+    log(`New user ${sanitizedMatric} added successfully`);
+    log(`Device "${req.bodyJson['device']['system']}" successfully registered`);
+    log(`User IP "${ipinfo['ip']}" successfully registered`);
+    return res.json({success: true, message: `New user ${sanitizedMatric} added successfully`});
   }
-
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
 };
+
+// TODO: ADD METHOD TO SEND DATA TO DISCORD TOO AS A NOTIFICATAION SYSTEM
