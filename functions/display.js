4c3ace3d1043f75be8b41baf45aa58155bf6b078
LightedExe <Thundermagicreal@gmail.com>
2024-11-25 11:40:17 +0800

    Forgot to return

--- functions/display.js
+++ functions/display.js
@@ -1,5 +1,5 @@
 export default ({ req, res, log, error }) => {
-    res.text("<h1>hello world</h1>", 200, {
+    return res.text("<h1>hello world</h1>", 200, {
         'Content-Type': 'text/html'
     })
 }
