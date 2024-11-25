3b89ec557f3bdb3f613b8d44903ea09ee2465448
LightedExe <Thundermagicreal@gmail.com>
2024-11-25 15:50:25 +0800

    completely removed check

--- functions/dataViewer/main.js
+++ functions/dataViewer/main.js
@@ -1,8 +1,4 @@
 export default ({ req, res, log, error }) => {
-    if (req.method != 'GET') {
-        error('Method not allowed: ' + req.method);
-        return res.json({success: false, message: "Method not allowed, Please send GET"});
-      }
     const html = `
 <!DOCTYPE html>
 <html lang="en">
