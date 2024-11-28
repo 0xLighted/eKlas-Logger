6180e5d2c1dc7a6ff32bc14b5c3aac53c07b60de
LightedExe <Thundermagicreal@gmail.com>
2024-11-28 14:48:10 +0800

    Removed all client based files

--- public/index.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { App } from './app';
-
-const renderApp = (config) => {
-    const root = createRoot(document.getElementById('root'));
-    root.render(<App config={config} />);
-};
-console.log("Render app in window context")
-window.renderApp = renderApp;
